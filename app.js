e72919c5b7f0826944f9b8f134f0784b78db2b7b
geracons <martin13rc@gmail.com>
2022-05-19 21:17:20 -0300

    fix port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Estamos corriendo en el puerto " + PORT);
 });
